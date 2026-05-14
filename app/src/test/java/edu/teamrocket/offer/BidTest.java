bfb6d6ddde174dd6b04b54f1aab05ecf9a94cb5e
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-05-14 09:19:32 +0200

    test(test): :white_check_mark: Añadir los casos test para Bid

---
+++ app/src/test/java/edu/teamrocket/offer/BidTest.java
@@ -0,0 +1,16 @@
+package edu.teamrocket.offer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BidTest {
+
+    @Test
+    void constructorTest() {
+        Bid bid = new Bid("9.5", 282);
+        assertEquals(282, bid.value());
+        assertEquals("9.5", bid.size());
+    }
+
+}
