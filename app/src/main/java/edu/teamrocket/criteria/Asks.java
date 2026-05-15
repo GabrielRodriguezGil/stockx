40a4e383a73a3e055ba0d990ec8863c05e4f3450
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-05-15 13:12:33 +0200

    feat(Asks): :sparkles: Implementar la funcionalidad completa Asks

--- app/src/main/java/edu/teamrocket/criteria/Asks.java
+++ app/src/main/java/edu/teamrocket/criteria/Asks.java
@@ -1,8 +1,10 @@
 package edu.teamrocket.criteria;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import edu.teamrocket.items.Item;
+import edu.teamrocket.offer.Ask;
 import edu.teamrocket.offer.Offer;
 
 public class Asks implements Criteria {
@@ -12,7 +14,7 @@ public class Asks implements Criteria {
 
     @Override
     public List<Offer> checkCriteria(Item item) {
-        return item.offers();
+        return item.offers().stream().filter(Ask.class::isInstance).collect(Collectors.toList());
     }
 
 }
